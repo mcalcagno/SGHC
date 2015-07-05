3150e7f15ab2daf83194bf8328d954e7dada88ca
saulfgr <saulfgr@VBOX-MC-PC>
2015-07-05 20:34:40 -0300

    se implementa sigleton de manejador

--- src/uy/com/sghc/persistencia/manejadores/ManejadorXml.java
+++ src/uy/com/sghc/persistencia/manejadores/ManejadorXml.java
@@ -1,5 +1,14 @@
 package uy.com.sghc.persistencia.manejadores;
 
 public class ManejadorXml {
-
+	private static ManejadorXml instance = null;
+	
+	private ManejadorXml(){};
+	
+	public static ManejadorXml newInstance() {
+		if (instance == null) {
+			instance = new ManejadorXml();
+		}		
+		return instance;
+	}
 }
